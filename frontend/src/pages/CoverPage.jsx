ae60d523eeefcad676682a133a6a107485cde743
Rushil Ranjan <96685832+Rushilr76@users.noreply.github.com>
2025-01-02 03:52:10 +0530

    Update CoverPage.jsx

--- frontend/src/pages/CoverPage.jsx
+++ frontend/src/pages/CoverPage.jsx
@@ -7,7 +7,7 @@ const CoverPage = () => {
     <section className="bg-gray-900 text-white py-20 md:py-48 w-full min-h-screen">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center text-center">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight mb-8">
-          Elevate Your Customer Experience with XENO
+          Elevate Your Customer Experience with us :)
         </h1>
         <p className="text-lg md:text-xl lg:text-2xl max-w-3xl mb-10">
           Empower your business with our CRM & Campaign Management tools to build lasting customer relationships and drive growth.
